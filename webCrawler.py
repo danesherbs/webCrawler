8721fddbad5b6324ec0070036532b9e2e4579ee2
dgs13 <dgs13@ic.ac.uk>
2016-02-04 21:38:17 +0000

    Finds all urls. Now going to do DFS instead

--- webCrawler.py
+++ webCrawler.py
@@ -1,68 +1,74 @@
 import requests
 from bs4 import BeautifulSoup
 import Queue
-import defaultdict
+from collections import defaultdict
+from urlparse import urljoin
+import re
 
+DOMAIN = 'gocardless.com'
+
+# Crawls a given url and produces a sitemap
 def webCrawler(url):
-    '''
-    Crawls a given url and produces a sitemap
+    # TODO: Only add links with host gocardless
+    # Use BFS to make it balanced. DFS would be easier but less readable.
 
-    Strategy:
-        - BFS search (add all unseen pages to queue)
-            - Only add links with host gocardless
-    '''
-
-    # TODO: check for valid URL; google.com doens't work but https://www.google.com does
-
-
-    # Needed for BFS:
+    # Needed for BFS
     unvisited = Queue.Queue(str)  # queue of unvisited links
     seen = defaultdict(bool)      # dictionary of seen links
+    seen[url] = True  # seen main page
 
-    # initialise queue and dictionary
-    links = getLinksOnPage(url)  # store links on given URL in list
-    addListToQueue(unvisited, links)  # initialise queue
-    addListToDictionary(seen, links)  # mark as each seen
+    # Start BFS on given URL
+    webCrawlerBFS(url, unvisited, seen)
 
+    # drawSitemap(seen.keys())
 
-    while (not unvisited.empty()):
-        print unvisited.get()
+    return None  # print a hierarchical tree structure?
 
+def webCrawlerBFS(url, unvisited, seen):
+    for subURL in getValidURLsOnPageWithinDomain(url):
+        print 'subURL', subURL
+        if not seen[subURL]:  # found a URL we haven't seen before!
+            seen[subURL] = True
+            unvisited.put(subURL)  # visit it later
+    if not unvisited.empty():
+        webCrawlerBFS(unvisited.get(), unvisited, seen)  # search next URL in queue
 
-def addListToDictionary(dictionary, links):  # TODO: think of a better name
-    '''
-    Adds elements of list to dictionary
-    '''
-    map(lambda link: dictionary[link]=True, links)
+def constructValidURL(url, link):
+    return urljoin(url, link)
 
-def addListToQueue(queue, links):
-    '''
-    Adds elements of list to queue
-    '''
-    map(queue.put, links)
-
-
-def getLinksOnPage(url):
-    '''
-    Input:  URL
-    Output: Exhaustive list of links present on page
-    '''
-    html = requests.get(url).text  # extract raw HTML
-    soup = BeautifulSoup(html,"lxml")
-    links = []
-    for link in soup.findAll('a', href=True):  # all anchors with href
+# Takes a URL and returns an exhaustive list of links present on page
+def getURLsOnPage(url):
+    # TODO: check for valid URL; google.com doens't work but https://www.google.com does
+    rawHTML = requests.get(url).text  # extract raw HTML
+    soup = BeautifulSoup(rawHTML,"lxml")  # used to parse raw HTML
+    urls = []  # list of urls present on page
+    for link in soup.findAll('a'):  # all anchors
         href = link.get('href')
-        if href != None and len(href) > 1:  # valid link
-            links.append(href)
-    return links
+        if href != None and len(href) > 1:  # non-empty link
+            url = urljoin(url, href)  # construct URL
+            urls.append(href)  # seen another URL!
+    return urls
 
+# Uses regex from Django to determine if URL is valid
+def validURL(url):
+    regex = re.compile(
+        r'^https?://'
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'
+        r'localhost|'
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        r'(?::\d+)?'
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return regex.search(url) is not None
 
-# webCrawler("https://gocardless.com/")
-# webCrawler("https://google.com/")
+# DOMAIN is in url
+def inDomain(url):
+    return DOMAIN in url
 
-GO_CARDLESS = "https://gocardless.com/"
+# Returns all sub-URLs on given URL that are both valid and in domain
+def getValidURLsOnPageWithinDomain(url):
+    return filter(lambda x: inDomain(x) and validURL(x), getURLsOnPage(url))
+
+GO_CARDLESS = "https://gocardless.com"
 
 webCrawler(GO_CARDLESS)
-
-# getLinksOnPage(GO_CARDLESS)
-# getLinksOnPage("http://tools.com/")
+# print getURLsOnPage(GO_CARDLESS)
