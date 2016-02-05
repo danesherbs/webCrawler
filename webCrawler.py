a792c41599648bd0fc6a347ed9bcc0f94eecc72e
Dane Sherburn <dane.sherburn13@imperial.ac.uk>
2016-02-05 10:23:00 +0000

    DFS working. Fix indentation

--- webCrawler.py
+++ webCrawler.py
@@ -9,14 +9,17 @@ DOMAIN = 'gocardless.com'
 
 # Crawls a given url and produces a sitemap
 def webCrawler(url):
-    seen = [url]
+    seen = [URLid(url)]
     webCrawlerDFS(url, seen)  # DFS on given URL
-    print sorted(seen)
+    seen = sorted(seen)
+    for seenURL in seen:
+        depth = Counter(seenURL)['/']
+        print depth * '\t' + seenURL
 
 def webCrawlerDFS(url, seen):
-    if len(seen) > 20:
+    if len(seen) > 100:
         return seen
-    for subURL in getValidURLsOnPageWithinDomain(url):
+    for subURL in generateSubURLs(url):
         subURLid = URLid(subURL)
         if subURLid in seen:
             continue  # seen URL before
@@ -25,18 +28,16 @@ def webCrawlerDFS(url, seen):
         webCrawlerDFS(subURL, seen)  # search this URL
 
 def URLid(url):
-    parsed = urlparse(url)  # avoid counting http and https separately
-    return parsed.netloc + parsed.path  # unique ID for URL
-
-# Joins base domain and relative link within domain to form a valid URL
-def constructValidURL(url, link):
-    return urljoin(url, link)
+    # TODO: fix - it includes www.gocardless.com and gocardless.com
+    url = url.split('/')[2:]
+    url = filter(lambda x: x!='', url)
+    return '/'.join(url)
 
 # Takes a URL and returns an exhaustive list of links present on page
 def getURLsOnPage(url):
     # TODO: check for valid URL; google.com doens't work but https://www.google.com does
     rawHTML = requests.get(url).text  # extract raw HTML
-    soup = BeautifulSoup(rawHTML,"lxml")  # used to parse raw HTML
+    soup = BeautifulSoup(rawHTML)  # used to parse raw HTML
     urls = []  # list of urls present on page
     for link in soup.findAll('a'):  # all anchors
         href = link.get('href')
@@ -47,10 +48,9 @@ def getURLsOnPage(url):
 
 # Uses regex from Django to determine if URL is valid (better than querying via HTTP)
 def validURL(url):
-    bannedSubstrings = ['#']
-    for substring in bannedSubstrings:
-        if substring in url:
-            return False
+    parsedURL = urlparse(url)
+    if parsedURL.query != '' or parsedURL.fragment != '':
+        return False  # no queries or fragments permitted
     regex = re.compile(
         r'^https?://'
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'
@@ -65,12 +65,11 @@ def inDomain(url):
     return DOMAIN in url
 
 # Returns all sub-URLs on given URL that are both valid and in domain
-def getValidURLsOnPageWithinDomain(url):
+def generateSubURLs(url):
     return filter(lambda x: inDomain(x) and validURL(x), getURLsOnPage(url))
 
 GO_CARDLESS = "https://gocardless.com"
-webCrawler(GO_CARDLESS)
-
+webCrawler('https://blog.gocardless.com')
 
 
 
