83a4850187126845233fbdc4ff06402de591c5c9
Dane Sherburn <dane.sherburn13@imperial.ac.uk>
2016-02-06 18:08:25 +0000

    Add write sitemap to file

--- webCrawler.py
+++ webCrawler.py
@@ -2,12 +2,15 @@ from tree import URLtree
 from utils import getLinksOnPage, getURL
 from Queue import Queue
 
-# Crawls a given url and produces a sitemap
+# Crawls a given URL and produces a sitemap
+# Output is stored in current directory under 'sitemap'
 def webCrawler(url):
     # TODO: only works for "https://gocardless.com"
     tree = webCrawlerDFS(url)
-    print '\n' * 3
-    print tree
+    sitemap = open('sitemap', 'w')
+    sitemap.write(str(tree))
+    sitemap.close()
+    print '\n\nSitemap saved in current directory.\n\n'
 
 def webCrawlerDFS(url):
     # initialise visited URLs
@@ -21,13 +24,13 @@ def webCrawlerDFS(url):
     # initialise tree
     tree = URLtree(url)
 
-    while(len(visited)<250):
+    while(len(visited)<10):
     # while(queue):
         link = queue.get()                        # next link
         if link not in visited:
             visited.add(link)                     # mark as seen
             map(queue.put, getLinksOnPage(link))  # visit new links later
-            print 'url_given', link
+            print link + ':',
             tree.insert(link)                     # put in tree hierarchy
 
     return tree
