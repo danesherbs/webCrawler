fa1753ec9f94f72781c39405097d7de6906c157b
Dane Sherburn <dane.sherburn13@imperial.ac.uk>
2016-02-06 18:16:30 +0000

    Wrap web crawler into object

--- webCrawler.py
+++ webCrawler.py
@@ -2,39 +2,36 @@ from tree import URLtree
 from utils import getLinksOnPage, getURL
 from Queue import Queue
 
-# Crawls a given URL and produces a sitemap
-# Output is stored in current directory under 'sitemap'
-def webCrawler(url):
-    # TODO: only works for "https://gocardless.com"
-    tree = webCrawlerDFS(url)
-    sitemap = open('sitemap', 'w')
-    sitemap.write(str(tree))
-    sitemap.close()
-    print '\n\nSitemap saved in current directory.\n\n'
+class WebCrawler(object):
 
-def webCrawlerDFS(url):
-    # initialise visited URLs
-    visited = set([getURL(url)])
+    # Crawls given URL and produces sitemap (stored in current directory)
+    def crawl(self, url):
+        tree = self.webCrawlerBFS(url)
+        sitemap = open('sitemap', 'w')
+        sitemap.write(str(tree))
+        sitemap.close()
+        print '\n\nSitemap saved in current directory.\n\n'
 
-    # initialise queue
-    queue = Queue()
-    for homeLink in getLinksOnPage(url):
-        queue.put(homeLink)
+    # Helper for crawl -- constructs trie via breadth first search
+    def webCrawlerBFS(self, url):
+        # initialise visited URLs
+        visited = set([getURL(url)])
+        # initialise queue
+        queue = Queue()
+        for homeLink in getLinksOnPage(url):
+            queue.put(homeLink)
+        # initialise tree
+        tree = URLtree(url)
+        while(len(visited)<10):
+            link = queue.get()                        # next link
+            if link not in visited:
+                visited.add(link)                     # mark as seen
+                map(queue.put, getLinksOnPage(link))  # visit new links later
+                print link + ':',
+                tree.insert(link)                     # put in tree hierarchy
+        return tree
 
-    # initialise tree
-    tree = URLtree(url)
-
-    while(len(visited)<10):
-    # while(queue):
-        link = queue.get()                        # next link
-        if link not in visited:
-            visited.add(link)                     # mark as seen
-            map(queue.put, getLinksOnPage(link))  # visit new links later
-            print link + ':',
-            tree.insert(link)                     # put in tree hierarchy
-
-    return tree
-
-
-GO_CARDLESS = "https://gocardless.com"
-webCrawler(GO_CARDLESS)
+if __name__ == '__main__':
+    GO_CARDLESS = "https://gocardless.com"
+    webCrawler = WebCrawler()
+    webCrawler.crawl(GO_CARDLESS)
